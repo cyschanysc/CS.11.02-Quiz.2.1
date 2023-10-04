5359563044666e33b988b63df03908b2a6516024
curtischan <curtischanyiksum@gmail.com>
2023-10-04 08:39:46 +0800

    this is completed

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
 public class Main {
-
     public static void main(String[] args) {
+        int resultOne = add(3, 5);
+        System.out.println(resultOne);
 
+        String resultTwo = greeting("Alice");
+        System.out.println(resultTwo);
+
+        int resultThree = add(2, 4, 6, 8);
+        System.out.println(resultThree);
+
+        printMe("Hello, world!");
     }
 
+    public static int add(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
+
+    public static String greeting(String name) {
+        return "Bonjour, " + name + "!";
+    }
+
+    public static int add(int numOne, int numTwo, int numThree, int numFour) {
+        int sumOne = add(numOne, numTwo);
+        int sumTwo = add(sumOne, numThree);
+        return add(sumTwo, numFour);
+    }
+
+    public static void printMe(String toBePrinted) {
+        System.out.println(toBePrinted);
+    }
 }
